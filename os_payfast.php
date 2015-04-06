c780ee80dc39d27bba900ccf00b13685bce83c73
sYk0 <wdcossey@gmail.com>
2015-04-06 19:42:06 +0200

    Included support for PayFast "passphrase" security feature. Generated md5 hash to send to PayFast (this was not needed until the use of "passphrase"). Fail subscription if (send & received) signatures don't match. Reordered parameters sent to PayFast (they are very particular about this). Removed commented code for recurring payments (not supported by PayFast),

--- os_payfast.php
+++ os_payfast.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version        1.0.0
+ * @version        1.2.0
  * @package        Joomla
  * @subpackage     Membership Pro
  * @author         William David Cossey
@@ -16,7 +16,7 @@ class os_payfast extends os_payment
 	/**
 	 * PayFast mode 
 	 *
-	 * @var boolean live mode : true, test mode : false
+	 * @var boolean live mode : true, sandbox : false
 	 */
 	var $_mode = 0;
 	
@@ -27,8 +27,18 @@ class os_payfast extends os_payment
 	 */
 	var $_url = null;
 	
-	//var $_passphrase = null;
+	/**
+	 * PayFast Passphrase (optional)
+	 *
+	 * @var string
+	 */
+	var $_passphrase = null;
 
+	/**
+	 * Submit Message
+	 *
+	 * @var string
+	 */
 	var $_submitMessage = 'Please wait while you are redirected to PayFast for payment processing...';
 	
 	/**
@@ -58,8 +68,8 @@ class os_payfast extends os_payment
 		parent::setCardType(false);
 		parent::setCardCvv(false);
 		parent::setCardHolderName(false);
-		$this->ipn_log = $params->get('payfast_debugmode');
-		$this->ipn_log_file = JPATH_COMPONENT . '/payfast_logs.txt';
+		$this->itn_log = $params->get('payfast_debugmode');
+		$this->itn_log_file = JPATH_COMPONENT . '/payfast_logs.txt';
 		$this->_mode = $params->get('payfast_mode');
 		
 		$tmpMessage = $params->get('message_submit');
@@ -72,15 +82,18 @@ class os_payfast extends os_payment
 		if ($this->_mode)
 		{
 			$this->_url = 'https://www.payfast.co.za/eng/process';
+			
 			$this->setParam('merchant_id', $params->get('merchant_id'));
 			$this->setParam('merchant_key', $params->get('merchant_key'));
-			//$this->_passphrase = $params->get('merchant_passphrase');
+			$this->_passphrase = $params->get('merchant_passphrase');
 		}
 		else
 		{
+			$this->_url = 'https://sandbox.payfast.co.za/eng/process';
+			
 			$this->setParam('merchant_id', $params->get('merchant_id_sandbox'));
 			$this->setParam('merchant_key', $params->get('merchant_key_sandbox'));
-			$this->_url = 'https://sandbox.payfast.co.za/eng/process';
+			$this->_passphrase = $params->get('merchant_passphrase_sandbox');
 		}
 	}
 
@@ -107,28 +120,33 @@ class os_payfast extends os_payment
 		$query = $db->getQuery(true);
 		$Itemid = JRequest::getInt('Itemid');
 		$siteUrl = JUri::base();
-		$this->setParam('item_name', $data['item_name']);
-		$this->setParam('amount', round($data['amount'], 2));
-		$this->setParam('custom_int1', $row->id);
+		
 		$query->select('*')
 			->from('#__osmembership_plans')
 			->where('id = '. $row->plan_id);
 		$db->setQuery($query);
 		$rowPlan = $db->loadObject();
-
-		$this->setParam('return_url', 
-			$siteUrl . 'index.php?option=com_osmembership&view=complete&Itemid=' . $Itemid);
+		
+		//PayFast is very particular about the order of parameters (esp. when generating a signature).
+		$this->setParam('return_url', $siteUrl . 'index.php?option=com_osmembership&view=complete&Itemid=' . $Itemid);
 		$this->setParam('cancel_url', $siteUrl . 'index.php?option=com_osmembership&view=cancel&id=' . $row->id . '&Itemid=' . $Itemid);
 		$this->setParam('notify_url', $siteUrl . 'index.php?option=com_osmembership&task=payment_confirm&payment_method=os_payfast');
+		
 		$this->setParam('name_first', $row->first_name);
 		$this->setParam('name_last', $row->last_name);
 		$this->setParam('email_address', $row->email);
+		$this->setParam('m_payment_id', $row->id);
+		$this->setParam('amount', round($data['amount'], 2));
+		$this->setParam('item_name', $data['item_name']);
+		//$this->setParam('item_description', );
 		
-		//$passPhrase = $this->_passphrase;
-		//if( !empty( $passPhrase ) && ($this->_mode) )
-		//{
-		//	$this->setParam('passphrase', urlencode(passPhrase));
-		//}
+		//$this->setParam('custom_int1', $row->id);
+		
+		$passPhrase = $this->_passphrase;
+		if( isset( $passPhrase ) )
+		{
+			$this->setParam('passphrase', urlencode($passPhrase));
+		}
 		
 		
 
@@ -145,98 +163,59 @@ class os_payfast extends os_payment
 		<div class="contentheading"><?php echo  JText::_($this->_submitMessage); ?></div>
 		<form method="post" action="<?php echo $this->_url; ?>" name="osm_form" id="osm_form">
 			<?php
+				$signatureString = null;
 				foreach ($this->_params as $key=>$val) 
 				{
-					echo '<input type="hidden" name="'.$key.'" value="'.$val.'" />';
-					echo "\n";	
+					if(!empty ( $val ))
+					{
+						$signatureString .= $key.'='.urlencode( trim( $val ) ).'&';
+						
+						if ( ( $key !== "passphrase" ))
+						{
+							echo '<input type="hidden" name="'.$key.'" value="'.urlencode( trim ( $val ) ).'" />';
+							echo "\n";
+						}
+					}
 				}
+				
+				// Remove last ampersand.
+				$signatureString = substr( $signatureString, 0, -1 );
+
+				// Update the signature.
+				$md5Hash = md5( $signatureString );				
 			?>
 			<script type="text/javascript">
 				function redirect() 
 				{
 					document.osm_form.submit();
 				}				
-				setTimeout('redirect()', 3000);
+				setTimeout('redirect()', 5000);
 			</script>
 		</form>
 	<?php	
 	}
-	/**
-	 * Validate the post data from PayFast to our server
-	 *
-	 * @return string
-	 */
-	/*function _validate()
-	{
-		$errNum = "";
-		$errStr = "";
-		$urlParsed = parse_url($this->_url);
-		$host = $urlParsed['host'];
-		$path = $urlParsed['path'];
-		$postString = '';
-		$response = '';
-		foreach ($_POST as $key => $value)
-		{
-			$this->_data[$key] = $value;
-			$postString .= $key . '=' . urlencode(stripslashes($value)) . '&';
-		}
-		$postString .= 'cmd=_notify-validate';
-		$fp = fsockopen($host, '80', $errNum, $errStr, 30);
-		if (!$fp)
-		{
-			return false;
-		}
-		else
-		{
-			fputs($fp, "POST $path HTTP/1.1\r\n");
-			fputs($fp, "Host: $host\r\n");
-			fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
-			fputs($fp, "Content-length: " . strlen($postString) . "\r\n");
-			fputs($fp, "Connection: close\r\n\r\n");
-			fputs($fp, $postString . "\r\n\r\n");
-			while (!feof($fp))
-			{
-				$response .= fgets($fp, 1024);
-			}
-			fclose($fp);
-		}
-		$this->ipn_response = $response;
-		$this->log_ipn_results(true);
-		if ($this->_mode)
-		{
-			if (eregi("VERIFIED", $response))
-				return true;
-			else
-				return false;
-		}
-		else
-		{
-			//Always return true for test mode, prevent unnecessary support requests	     	
-			return true;
-		}
-	}*/
 
 	/**
-	 * Log IPN result
+	 * Log ITN result
 	 *
 	 * @param string $success
 	 */
-	function log_ipn_results($success)
+	function log_itn_results($success)
 	{
-		if (!$this->ipn_log)
+		if (!$this->itn_log)
 			return;
 		$text = '[' . date('m/d/Y g:i A') . '] - ';
 		if ($success)
 			$text .= "SUCCESS!\r\n";
 		else
 			$text .= 'FAIL: ' . $this->last_error . "\n";
-		$text .= "IPN POST Variables from PayFast:\r\n";
+		$text .= "ITN POST Variables from PayFast:\r\n";
 		foreach ($this->_data as $key => $value)
 		{
 			$text .= "$key=$value, ";
 		}
-		$text .= "\r\nIPN Response from PayFast Server:\r\n " . $this->ipn_response;
-		$fp = fopen($this->ipn_log_file, 'a');
+		$text .= "\r\nITN Response from PayFast Server:\r\n " . $this->itn_response;
+		$fp = fopen($this->itn_log_file, 'a');
 		fwrite($fp, $text . "\r\n\r\n");
 		fclose($fp); // close file
 	}
@@ -269,7 +248,7 @@ class os_payfast extends os_payment
 		// Variable initialization
 		$pfError = false;
 		$pfErrMsg = '';
-		$filename = $this->ipn_log_file;
+		$filename = $this->itn_log_file;
 		$output = '';
 		$pfParamString = '';
 		
@@ -308,11 +287,11 @@ class os_payfast extends os_payment
 			$pfTempParamString = $pfParamString;
 			 
 			// If a passphrase has been set in the PayFast Settings, then it needs to be included in the signature string.
-			//$passPhrase = $this->_passphrase; //You need to get this from a constant or stored in you website
-			//if( !empty( $passPhrase ) && ($this->_mode) )
-			//{
-			//	$pfTempParamString .= '&passphrase='.urlencode( $passPhrase );
-			//}
+			$passPhrase = $this->_passphrase;
+			if( !empty( $passPhrase ) )
+			{
+				$pfTempParamString .= '&passphrase='.urlencode( $passPhrase );
+			}
 			
 			$signature = md5( $pfTempParamString );
 			
@@ -324,6 +303,13 @@ class os_payfast extends os_payment
 			$output .= "- posted     = ". $pfData['signature'] ."\r\n"; // DEBUG
 			$output .= "- calculated = ". $signature ."\r\n"; // DEBUG
 			$output .= "- result     = ". ( $result ? 'SUCCESS' : 'FAILURE' ) ."\r\n"; // DEBUG
+			
+			if( $result === false )
+			{
+				$pfError = true;
+				$pfErrMsg = PF_ERR_INVALID_SIGNATURE;
+			}
+			$pfError = !$result;
 		}
 		 
 		//// Verify source IP
@@ -475,8 +461,8 @@ class os_payfast extends os_payment
 			$output .= "\r\nError = ". $pfErrMsg;
 		}
 		
-		$this->ipn_response = $output;
-		$this->log_ipn_results(!$pfError);
+		$this->itn_response = $output;
+		$this->log_itn_results(!$pfError);
 		
 		return !$pfError;
 	}
@@ -493,7 +479,7 @@ class os_payfast extends os_payment
 		{
 			$config = OSMembershipHelper::getConfig();
 			$row = JTable::getInstance('OsMembership', 'Subscriber');
-			$id = $this->_data['custom_int1'];
+			$id = $this->_data['m_payment_id'];
 			$transactionId = $this->_data['txn_id'];
 			
 			if ($transactionId && OSMembershipHelper::isTransactionProcessed($transactionId))
@@ -542,50 +528,8 @@ class os_payfast extends os_payment
 	 */
 	/*function processRecurringPayment($row, $data)
 	{
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		$siteUrl = JUri::base();
-		$Itemid = JRequest::getInt('Itemid');
-		$query->select('*')
-			->from('#__osmembership_plans')
-			->where('id = '. $row->plan_id);
-		$db->setQuery($query);
-		$rowPlan = $db->loadObject();
-
-		$this->setParam('item_name', $data['item_name']);
-		$this->setParam('custom', $row->id);
-
-		$this->setParam('return', $siteUrl . 'index.php?option=com_osmembership&view=complete&Itemid=' . $Itemid);
-
-		$this->setParam('cancel_return', $siteUrl . 'index.php?option=com_osmembership&view=cancel&id=' . $row->id . '&Itemid=' . $Itemid);
-		$this->setParam('notify_url', $siteUrl . 'index.php?option=com_osmembership&task=recurring_payment_confirm&payment_method=os_payfast');
-		$this->setParam('cmd', '_xclick-subscriptions');
-		$this->setParam('src', 1);
-		$this->setParam('sra', 1);
-		$this->setParam('a3', $data['regular_price']);
-		$this->setParam('address1', $row->address);
-		$this->setParam('address2', $row->address2);
-		$this->setParam('city', $row->city);
-		$this->setParam('country', $row->country);
-		$this->setParam('first_name', $row->first_name);
-		$this->setParam('last_name', $row->last_name);
-		$this->setParam('state', $row->state);
-		$this->setParam('zip', $row->zip);
-		$this->setParam('p3', $rowPlan->subscription_length);
-		$this->setParam('t3', $rowPlan->subscription_length_unit);
-		$this->setParam('lc', 'US');
-		if ($rowPlan->number_payments > 1)
-		{
-			$this->setParam('srt', $rowPlan->number_payments);
-		}
-		if ($rowPlan->trial_duration)
-		{
-			$this->setParam('a1', $data['trial_amount']);
-			$this->setParam('p1', $rowPlan->trial_duration);
-			$this->setParam('t1', $rowPlan->trial_duration_unit);
-		}
-		//Redirect users to PayFast for processing payment
-		$this->submitPost();
+		PayFast doesn't support recurring payments
+		https://www.payfast.co.za/support/index.php?/Knowledgebase/Article/View/14/3/can-i-receive-recurring-payments
 	}*/
 	
 	/**
@@ -594,52 +538,6 @@ class os_payfast extends os_payment
 	 */
 	/*function verifyRecurringPayment()
 	{
-		$ret = $this->_validate();
-		if ($ret)
-		{
-			$config = OSMembershipHelper::getConfig();
-			$id = $this->_data['custom'];
-			$transactionId = $this->_data['txn_id'];
-			$amount = $this->_data['mc_gross'];
-			$txnType = $this->_data['txn_type'];
-			if ($amount < 0)
-			{
-				return false;
-			}
-			if ($transactionId && OSMembershipHelper::isTransactionProcessed($transactionId))
-			{
-				return false;
-			}
-			$row = JTable::getInstance('OsMembership', 'Subscriber');
-			switch ($txnType)
-			{
-				case 'subscr_signup':
-					$row->load($id);
-					if (!$row->id)
-					{
-						return false;
-					}
-					if (!$row->published)
-					{
-						$row->transaction_id = $transactionId;
-						$row->payment_date = date('Y-m-d H:i:s');
-						$row->published = 1;
-						$row->store();
-						OSMembershipHelper::sendEmails($row, $config);
-						JPluginHelper::importPlugin('osmembership');
-						$dispatcher = JDispatcher::getInstance();
-						$dispatcher->trigger('onMembershipActive', array($row));
-					}
-					break;
-				case 'subscr_payment':
-					OSMembershipHelper::extendRecurringSubscription($id);
-					break;
-			}
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		PayFast doesn't support recurring payments
 	}*/
 }
